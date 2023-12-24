c8d9796bd450bac2e2466c74654cb5d990dae181
panbk0128 <1575232960@qq.com>
2023-12-24 23:14:16 +0800

    s

--- DXWL_DATA/dxwl_analysis.py
+++ DXWL_DATA/dxwl_analysis.py
@@ -3,16 +3,16 @@ import pandas as pd
 df = pd.read_csv('dxwl.csv')
 # print(df.head())
 # print(df.shape)
-df.sort_values(by='时间')
+df.sort_values('时间')
 df_dropTeacher = df.drop(df[(df['账号'] < 20000000000)].index)
 
 # 构造数据的header
-for x, y in df_dropTeacher.groupby(by=['姓名']):
+for x, y in df_dropTeacher.groupby('姓名'):
     list_temp = []
     list_temp.append('时间')
     list_temp.append('姓名')
     list_temp.append('账号')
-    for z, s in y.groupby(by=['事件类型']):
+    for z, s in y.groupby('事件类型'):
         list_temp.append(z.strip('\t'))
     break
 
@@ -48,10 +48,10 @@ for i in range(len(date_range1)):
 
     #     print(df_M.shape)
     stu_number = df_M['姓名'].nunique()  # 学习人数
-    print(stu_number)
+    # print(stu_number)
     # 平均活动次数:
     ave_activity = df_M.shape[0] / stu_number
-    print("平均活动次数:" + str(int(ave_activity)))
+    # print("平均活动次数:" + str(int(ave_activity)))
 
     # 获取每个人的活动次数 活跃人数  活跃率
     date = date_range1[i]
@@ -71,15 +71,15 @@ for i in range(len(date_range1)):
 
     #          if(times >10):
     #             break
-    print('第{}月的互动人数:{}'.format(i, ac_number))
-    print('第{}月的活跃人数:{}'.format(i, huoyue_number))
-    print('第{}月的活跃率:{}'.format(i, huoyue_ratio))
+    # print('第{}月的互动人数:{}'.format(i, ac_number))
+    # print('第{}月的活跃人数:{}'.format(i, huoyue_number))
+    # print('第{}月的活跃率:{}'.format(i, huoyue_ratio))
 
     # 统计表 index:
     list_temp = [date.strftime('%Y/%m'), ac_number, huoyue_number, ac_sum, huoyue_ratio]
     df_list.append(list_temp)
-    df_activity = pd.DataFrame(df_list, columns=['日期(月)', '参与学生人数', '活跃人数', '学习活动总数', '学生活跃率'])
-    print(df_activity)
+df_activity = pd.DataFrame(df_list, columns=['日期(月)', '参与学生人数', '活跃人数', '学习活动总数', '学生活跃率'])
+print(df_activity)
 
 # 区分不同的行为：学习行为和缺席
 # dict_activit = {'获得考试成绩':1,'参与签到':2,'学生访问章节':3,'完成任务点':4,
@@ -101,44 +101,6 @@ for i in range(len(date_range1)):
 # print(df_activity.shape)
 
 
-
-
-""" 统计每个班级的学生学习情况"""
-list_new = []
-for name1, info in df_dropTeacher.groupby('班级名'):
-    #     print(name)
-
-    # 班级
-
-    # 每个月的活动次数
-
-    for i in range(len(date_range1)):
-        if (i == 0):
-            df_M = info[info['时间'] < date_range1[i]]
-        else:
-            df_M = info[(info['时间'] < date_range1[i]) & (info['时间'] > date_range1[i - 1])]
-        #         print(df_M)
-
-        #   日期
-        list_cl = []
-        list_cl.append(name1)
-        list_cl.append(date_range1[i].strftime('%Y/%m'))
-        jiaohu_person = df_M['姓名'].nunique()
-        jiaohu_count = df_M.shape[0]
-        ave_jiaohu = (int)(jiaohu_count / jiaohu_person)
-        huoyue_person = 0;
-        for name2, info1 in df_M.groupby('姓名'):
-            if (info1['事件类型'].count() > ave_jiaohu):
-                huoyue_person = huoyue_person + 1
-        # 活跃人数
-        # 活跃率
-        list_cl.append(jiaohu_person)
-        list_cl.append(format(huoyue_person / jiaohu_person, '.2f'))
-        list_cl.append(df_M.shape[0])
-        print(list_cl)
-        list_new.append(list_cl)
-df_new = pd.DataFrame(list_new, columns=['班级', '日期', '活跃人数', '活跃率', '总活动次数'])
-df_new
 
 
 
@@ -174,7 +136,7 @@ for name1, info in df_dropTeacher.groupby('班级名'):
         list_cl.append(jiaohu_person)
         list_cl.append(format(huoyue_person / jiaohu_person, '.2f'))
         list_cl.append(df_M.shape[0])
-        print(list_cl)
+        # print(list_cl)
         list_new.append(list_cl)
 df_new = pd.DataFrame(list_new, columns=['班级', '日期', '活跃人数', '活跃率', '总活动次数'])
-df_new
+print(df_new)
